d7597c3c330776e9c372cb711f15b74c031801ef
Sachin Bhone <sachinbhone40@gmail.com>
2024-12-20 15:02:26 +0530

    EP-10 tailwind CSS added

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -8,9 +8,13 @@ const RestaurantCard = (props) => {
   const { slaString } = resData?.info?.sla;
 
   return (
-    <div className="res-card">
-      <img alt="res logo" src={CDN_URL + cloudinaryImageId} />
-      <h3>{name}</h3>
+    <div className="m-4 p-4 w-[250px] bg-gray-100 rounded-lg hover:bg-gray-200">
+      <img
+        className="rounded-lg"
+        alt="res logo"
+        src={CDN_URL + cloudinaryImageId}
+      />
+      <h3 className="font-bold py-2 text-lg">{name}</h3>
       <h4>{cuisines.join(", ")}</h4>
       <h4>{avgRating} stars</h4>
       <h4>{costForTwo}</h4>
